e9a03a503f2225e8b7a20a35d973716e711b37f0
Cristian Gonzalez Sierra <cristiangs777@gmail.com>
2022-10-23 16:53:40 +0200

    les09-3 complete

--- les09/Sequence.java
+++ les09/Sequence.java
@@ -5,14 +5,27 @@ public class Sequence {
     int secondNumber = 1;
 
     public void displaySequence() {
+        //while loop
         System.out.print(firstNumber + " " + secondNumber + " ");
         nextNumber = firstNumber + secondNumber;
-        while(nextNumber<=secuenceLimit){
-            System.out.print(nextNumber+" ");
-            firstNumber=secondNumber;
-            secondNumber=nextNumber;
-            nextNumber=firstNumber+secondNumber;
+        while (nextNumber <= secuenceLimit) {
+            System.out.print(nextNumber + " ");
+            firstNumber = secondNumber;
+            secondNumber = nextNumber;
+            nextNumber = firstNumber + secondNumber;
         }
         System.out.println();
+        //for loop
+        final int sequenceCount = 10;
+        firstNumber = 0;
+        secondNumber = 1;
+        nextNumber = firstNumber + secondNumber;
+        System.out.print(firstNumber + " " + secondNumber + " ");
+        for (int i = 2; i < sequenceCount; i++) {
+            System.out.print(nextNumber + " ");
+            firstNumber = secondNumber;
+            secondNumber = nextNumber;
+            nextNumber = firstNumber + secondNumber;
+        }
     }
 }
