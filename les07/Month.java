ca1ae8da851c4593593e40f4ee6e737685addcf4
Cristian Gonzalez Sierra <cristiangs777@gmail.com>
2022-10-23 16:22:16 +0200

    les07-2 complete

--- les07/Month.java
+++ les07/Month.java
@@ -1,5 +1,47 @@
 public class Month {
-    public void displayMonth(){
+    int monthNumber = 1;
 
+    public void displayMonth() {
+        switch (monthNumber) {
+            case 1:
+                System.out.println("Enero");
+                break;
+            case 2:
+                System.out.println("Febrero");
+                break;
+            case 3:
+                System.out.println("Marzo");
+                break;
+            case 4:
+                System.out.println("Abril");
+                break;
+            case 5:
+                System.out.println("Mayo");
+                break;
+            case 6:
+                System.out.println("Junio");
+                break;
+            case 7:
+                System.out.println("Julio");
+                break;
+            case 8:
+                System.out.println("Agosto");
+                break;
+            case 9:
+                System.out.println("Septiembre");
+                break;
+            case 10:
+                System.out.println("Octubre");
+                break;
+            case 11:
+                System.out.println("Nobiembre");
+                break;
+            case 12:
+                System.out.println("Diciembre");
+                break;
+            default:
+                System.out.println("invalido");
+
+        }
     }
 }
