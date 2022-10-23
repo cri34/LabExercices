2235059504d0d37e5e311c8f3ad00654f8dba3d9
Cristian Gonzalez Sierra <cristiangs777@gmail.com>
2022-10-23 16:52:02 +0200

    les09-2 complete

--- les09/Sequence.java
+++ les09/Sequence.java
@@ -1,5 +1,18 @@
 public class Sequence {
-    public void displaySequence(){
+    int nextNumber;
+    final int secuenceLimit = 100;
+    int firstNumber = 0;
+    int secondNumber = 1;
 
+    public void displaySequence() {
+        System.out.print(firstNumber + " " + secondNumber + " ");
+        nextNumber = firstNumber + secondNumber;
+        while(nextNumber<=secuenceLimit){
+            System.out.print(nextNumber+" ");
+            firstNumber=secondNumber;
+            secondNumber=nextNumber;
+            nextNumber=firstNumber+secondNumber;
+        }
+        System.out.println();
     }
 }
